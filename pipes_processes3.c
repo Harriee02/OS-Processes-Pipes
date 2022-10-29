2b2b389ca61bc6dfbd69d828515b0cf08494b7ac
Taiwo Oriowo <taiwo.oriowo@bison.howard.edu>
2022-10-29 21:41:47 +0000

    Final lab push

--- pipes_processes3.c
+++ pipes_processes3.c
@@ -18,9 +18,10 @@ int main(int argc, char **argv)
 	int pipefd1[2];
   int pid;
 	int pid2;
+  int status;
 
   char *cat_args[] = {"cat", "scores", NULL};
-  char *grep_args[] = {"grep", "Lakers", NULL};
+  char *grep_args[] = {"grep", argv[1], NULL};
 
   // make a pipe (fds go in pipefd[0] and pipefd[1])
 
@@ -31,32 +32,27 @@ int main(int argc, char **argv)
 
   if (pid == 0)
     {
-			dup2(pipefd1[0], 0);
-			close(pipefd1[1]);
-
 			pid2 = fork();
 
 			if (pid2 == 0){
-				
-				dup2(pipefd1[0], 0);
-        close(pipefd1[1]);
-
-				char* sort_arg[] = {"sort", NULL};
-				execvp("sort", sort_arg);
+				dup2(pipefd1[1], 1);
+        close(pipefd1[0]);
+				close(pipefd[0]);
+				close(pipefd[1]);
+				execvp("cat", cat_args);
 			}
 			else{
 				// child gets here and handles "grep Villanova"
 
       // replace standard input with input part of pipe
 
-      dup2(pipefd1[1], 1);
-
+      dup2(pipefd1[0], 0);
+			dup2(pipefd[1], 1);
       // close unused hald of pipe
-			close(pipefd1[0]);
-
+			close(pipefd1[1]);
+			close(pipefd[0]);
       // execute grep
-
-      execvp("grep", argv);
+      execvp("grep", grep_args);
 			}
       
     }
@@ -65,15 +61,19 @@ int main(int argc, char **argv)
       // parent gets here and handles "cat scores"
 
       // replace standard output with output part of pipe
+      
 
-      dup2(pipefd[1], 1);
+      dup2(pipefd[0], 0);
 
       // close unused unput half of pipe
 
-      close(pipefd[0]);
+      close(pipefd[1]);
+			close(pipefd1[1]);
+			close(pipefd1[0]);
 
       // execute cat
-
-      execvp("cat", cat_args);
+      char* sort_arg[] = {"sort", NULL,NULL};
+			execvp("sort", sort_arg);
+      
     }
 }
